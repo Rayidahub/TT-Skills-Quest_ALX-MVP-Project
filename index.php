256db040dd0a188936d553b4ed7ac871199b380c
Raymond Gaius Yidawi <rayidatech@gmail.com>
2023-07-08 17:26:18 -0500

    Updated

--- index.php
+++ index.php
@@ -200,6 +200,7 @@ if(isset($_GET['logout'])){
                     <div class="btn-group">
                         <a href="https://drive.google.com/file/d/1CpEsmkclByprRFU7HccHQ4DlYjLn6peF/view?usp=sharing" class="btn active"> Download CV</a>
                         <a href="https://wa.link/bk7bne" class="btn">Let's Chat</a>
+                        <a href="https://wa.link/bk7bne" class="btn">GitHub Project</a>
                     </div>
                     
                     </div>
